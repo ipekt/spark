9ae23bf4fe43a2d97381d6cc6e7c64ed897e71ab
Ipek Turgul <ipek.turgul@gmail.com>
2017-10-14 10:44:13 +1100

    master Add firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+import config from './firebase-config';
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
